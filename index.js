86ace3f73b80f575f38678dc411123959dce6f52
Krishna Manoj PVR <pvrkmsbunny.com>
2024-04-17 09:24:16 +0530

    CONSOLELOGS

--- index.js
+++ index.js
@@ -7,38 +7,40 @@ app.set("views", path.join(__dirname, "views"));
 
 // OUR ROUTES WILL GO HERE
 app.get("/", (req, res) => {
+	console.log("/ ROUTE");
 	return res.render("index");
 });
 app.get("/download", async (req, res) => {
+	console.log("/download ROUTE");
+	if (!req.query.url) {
+		return res.status(400).send('URL query parameter is required');
+	}
 	const v_id = req.query.url.split('v=')[1];
-    const info = await ytdl.getInfo(req.query.url);
+	if (!v_id) {
+		return res.status(400).send('Invalid URL');
+	}
+    const info = await ytdl.getInfo(req.query.url);	
+	console.log(info)
 	const filteredFormats = info.formats.filter(format => {
         return format.hasAudio && format.hasVideo && format.container === 'mp4';
     });
+	console.log(filteredFormats)
 	// Find the format with the highest resolution
     const highestResolutionFormat = filteredFormats.reduce((prev, current) => {
         return (parseInt(prev.height) > parseInt(current.height)) ? prev : current;
     });
-// 1
-	// return res.render("download", {
-	// 	url: "https://www.youtube.com/embed/" + v_id,
-    //     // info: info.formats.sort((a, b) => {
-    //     //     return a.mimeType < b.mimeType;
-	// 	info: [highestResolutionFormat],
-    //     // }),
-	// });
-// 2
-	// res.redirect(highestResolutionFormat.url);
+	console.log(highestResolutionFormat)
 
-	// 3
 	 // Set the appropriate headers for the file download
 	 res.setHeader('Content-disposition', 'attachment; filename=' + v_id + '.mp4');
 	 res.setHeader('Content-type', 'video/mp4');
+	 console.log("headers are set");
  
 	 // Pipe the video stream to the response
 	 ytdl(req.query.url, {
 		 format: highestResolutionFormat
 	 }).pipe(res);
+	 console.log("piping started");
 });
 app.listen(3000, () => {
 	console.log("Server is running on http://localhost:3000");
